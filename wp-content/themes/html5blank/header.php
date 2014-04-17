f2dbfccbc9cea454c7b6886b3689944c8752c056
jiroume81 <jiroume81@gmail.com>
2014-04-17 15:25:59 +0200

    commit 3

--- wp-content/themes/html5blank/header.php
+++ wp-content/themes/html5blank/header.php
@@ -33,9 +33,9 @@
 
 					<!-- logo -->
 					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
+						<a href="http://localhost/206istes">
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							<img src="http://up.autotitre.com/30169992e0.jpg" alt="Logo" class="logo-img">
 						</a>
 					</div>
 					<!-- /logo -->
